c4411407763b6eaffd9019885f2419c848b6e50d
Antonio Stassi <stassiant@outlook.it>
2024-10-29 18:27:56 +0100

    TypeSeeder add on DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ProjectSeeder::class
+            ProjectSeeder::class,
+            TypeSeeder::class
         ]);
     }
 }
